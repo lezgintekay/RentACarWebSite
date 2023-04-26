20e4ef6f3e36d1f34552c3c50397e34bd45bf808
lezgintekay <85061866+lezgintekay@users.noreply.github.com>
2023-04-26 18:21:08 +0300

    fix sql error

--- araclarimiz.php
+++ araclarimiz.php
@@ -9,10 +9,10 @@ include 'config.php';
    <title>Oto Galeri/Araçlar</title>
 
 
-   <!-- Fontlar -->
+
    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
 
-   <!-- Bootstrap stil dosyası -->
+
    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
    <!-- işimize yarayacak diğer kütüphane css dosyalarımız -->
@@ -23,7 +23,7 @@ include 'config.php';
    <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
    <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
 
-   <!-- bizim stil dosyamız -->
+
    <link href="css/style.css" rel="stylesheet">
 
    <style>
@@ -53,7 +53,7 @@ include 'config.php';
 
 <body id="body">
 
-   <!-- ÜST BAR -->
+   <!-- TOP BAR -->
 
    <section id="topbar" class="d-none d-lg-block">
       <div class="container clearfix">
@@ -142,7 +142,7 @@ include 'config.php';
    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
 
-   <!-- Kütüphaneler -->
+
    <script src="lib/jquery/jquery.min.js"></script>
    <script src="lib/jquery/jquery-migrate.min.js"></script>
    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
